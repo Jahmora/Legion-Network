ade4e2862484d41a5cfb15fb11b631cee4fe7b25
Jahmora <Miphonej13@gmail.com>
2025-03-09 02:30:41 +0100

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const translations = {
   en: {
     commands: {
@@ -14,15 +16,23 @@ const translations = {
     messages: {
       consentQuestion: "By using this service for mining $Legion, you consent to use Legion Network's services.",
       welcome: "Welcome to Legion Network! Type 'help' to get started.",
-      limitedCommands: "Available commands: help, installer.",
+      limitedCommands: "Available commands now: help, installer.",
       availableCommands: "Available commands: help, clear, exit, installer, dig, build node, build road, state, quit.",
       installationStep1: "Downloading files...",
       installationStep2: "Extracting resources...",
       installationStep3: "Installing dependencies...",
       installationComplete: "Installation complete!",
-      postInstallationInstructions: "You can now interact with the system. Use 'dig' to mine blocks, 'build node' to set up a blockchain node, or 'help' to see all commands.",
-      commandReceived: "You executed: {command}",
-      commandNotRecognized: "Command not recognized. Type 'help' for the list of commands."
+      postInstallationInstructions: "You may now interact with the system. Use 'dig' to mine blocks, 'build node' to build a blockchain node, or 'help' to see all commands.",
+      diggingMessage: "You have dug and gained 5 blocks.",
+      buildNodeSuccess: "You have built a blockchain node!",
+      buildRoadSuccess: "You have built a road.",
+      insufficientResourcesNode: "Insufficient resources to build a node.",
+      insufficientResourcesRoad: "Insufficient resources to build a road.",
+      stateDisplay: "Current state: Energy = {energy}, Blocks = {blocks}",
+      constructionsDisplay: "Constructions: Nodes = {nodes}, Roads = {roads}",
+      quitMessage: "You have exited the colonization simulation.",
+      commandNotRecognized: "Command not recognized. Type 'help' for the list of commands.",
+      commandReceived: "You executed: {command}"
     }
   },
   fr: {
@@ -40,15 +50,23 @@ const translations = {
     messages: {
       consentQuestion: "En utilisant ce service pour le minage de $Legion, vous consentez à utiliser les services de Legion Network.",
       welcome: "Bienvenue dans le réseau Legion ! Tapez 'aide' pour commencer.",
-      limitedCommands: "Commandes disponibles : aide, installer.",
+      limitedCommands: "Commandes disponibles actuellement : aide, installer.",
       availableCommands: "Commandes disponibles : aide, clear, exit, installer, creuser, batir noeud, construire route, etat, quitter.",
       installationStep1: "Téléchargement des fichiers...",
       installationStep2: "Extraction des ressources...",
       installationStep3: "Installation des dépendances...",
       installationComplete: "Installation terminée !",
-      postInstallationInstructions: "Vous pouvez maintenant interagir avec le système. Utilisez 'creuser' pour miner des blocs, 'batir noeud' pour établir un noeud blockchain, ou 'aide' pour voir toutes les commandes.",
-      commandReceived: "Vous avez exécuté : {command}",
-      commandNotRecognized: "Commande non reconnue. Tapez 'aide' pour voir la liste des commandes."
+      postInstallationInstructions: "Vous pouvez maintenant interagir avec le système. Utilisez 'creuser' pour miner des blocs, 'batir noeud' pour bâtir un nœud blockchain, ou 'aide' pour voir toutes les commandes.",
+      diggingMessage: "Vous avez creusé et gagné 5 blocs.",
+      buildNodeSuccess: "Vous avez bâti un nœud blockchain !",
+      buildRoadSuccess: "Vous avez construit une route.",
+      insufficientResourcesNode: "Ressources insuffisantes pour bâtir un nœud.",
+      insufficientResourcesRoad: "Ressources insuffisantes pour construire une route.",
+      stateDisplay: "État actuel : Energie = {energy}, Blocs = {blocks}",
+      constructionsDisplay: "Constructions : Nœuds = {nodes}, Routes = {roads}",
+      quitMessage: "Vous avez quitté la simulation de colonisation.",
+      commandNotRecognized: "Commande non reconnue. Tapez 'aide' pour voir la liste des commandes.",
+      commandReceived: "Vous avez exécuté : {command}"
     }
   }
 };
